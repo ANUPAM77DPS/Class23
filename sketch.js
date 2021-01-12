1c85aa063f8cf4455be606434473ff8c6aa9711e
ANUPAM77DPS <75565181+ANUPAM77DPS@users.noreply.github.com>
2021-01-12 19:11:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, ball;
+
+function setup(){
+    var canvas = createCanvas(600,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(300,390,600,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 1.0
+    }
+
+    ball = Bodies.circle(200,100,20, ball_options);
+    World.add(world,ball);
+
+    console.log(ground);
+    b1=new Box(100,200,20,20)
+    b2=new Box(400,100,30,30) 
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,600,20);
+
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
+    b1.display()
+    b2.display()
+}
